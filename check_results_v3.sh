4df463a6de2d2ff39566bf345b149b0b5049d6fd
Henry LETELLIER <henry.letellier@epitech.eu>
2023-03-20 22:35:55 +0100

    [FIXED] the error output (maiby)

---
+++ check_results_v3.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# (c) Written by Henry Letellier
+CONTENT="$(cat coding-style-reports.log)"
+ILLEGAL_FILES_FOUND=0
+while IFS= read -r LINE; do
+    FILE=$(echo $LINE | cut -d ':' -f 1)
+    LINE_NUMBER=$(echo $LINE | cut -d ':' -f 2)
+    echo "::error file=$FILE,line=$LINE_NUMBER,endLine=0,title=MAJOR coding style error::$FILE#L$LINE_NUMBER"
+    ILLEGAL_FILES_FOUND=1
+done <<<"$CONTENT"
+if [ $ILLEGAL_FILES_FOUND -eq 1 ]; then
+    exit 1
+else
+    echo "::success file='',line=1,endLine=0,title=No Unwanted file detected"
+fi
