5b45fcb5bbb404282d2f1b72847be186c7cf5292
Henry LETELLIER <henry.letellier@epitech.eu>
2023-03-23 21:15:54 +0100

    [ADDED] files to make the coding-style test fail

---
+++ bonus/clean_code/include/my.h
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2022
+** chocolatine (Workspace)
+** File description:
+** my.h
+*/
+
+#ifndef MY_H_
+    #define MY_H_
+
+int my_strlen(char const *str);
+void my_putstr(char const *str);
+
+#endif
---
+++ bonus/clean_code/test.c
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2022
+** chocolatine (Workspace)
+** File description:
+** test.c
+*/
+
+#include "my.h"
+
+int main(void)
+{
+    my_putstr("Hello World\n");
+    return 0;
+}
